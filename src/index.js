2d7c609ff7a453a425e9f17e9a6aaf39edf6d0ae
Diogo Augusto <diogo.augustoo@gmail.com>
2019-10-11 17:29:04 -0300

    FIX - Deployment HEROKU

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
